e3fc9f7144d0573466627d0bad3aee8dcb27e0ab
imsochill <cool@d-laptop.localdomain>
2020-09-23 20:16:39 +0100

    Allow install.sh to be called with sudo but install for caller user

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #! /usr/bin/env bash
 
+# The user that called the script.
+user=$(logname)
+
 # The dotfiles directory.
 directory="$PWD"
 
 # The symlinks file.
 bindings=bindings
 
-while read -r file link; do
+while read -r file destination; do
     filepath="$directory/$file"
+    link="${destination//\~/\~$user}"
     echo "Binding $link to file $filepath"
     ln -sfn "$filepath" $(eval echo "$link")
 done < $bindings
